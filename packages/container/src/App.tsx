25daafbc62f68102dc8101b31ff903a81297c835
jamesabels <james.abels@tangramflex.com>
2021-05-17 13:00:20 -0400

    Format App.tsx

--- packages/container/src/App.tsx
+++ packages/container/src/App.tsx
@@ -14,34 +14,34 @@ import '@blueprintjs/icons/lib/css/blueprint-icons.css'
 import '@blueprintjs/core/lib/css/blueprint.css'
 
 export default function App() {
-  return (
-    <Suspense fallback={<h1>Loading...</h1>}>
-      <Router>
-        <div className="grid-container">
-          <div className="nav">
-            <Nav />
-          </div>
-          <div className="sidebar">
-            <Sidebar />
-          </div>
-          <div className="stage">
-            <Switch>
-              <Route exact path="/">
-                <Dashboard />
-              </Route>
-              <Route path="/workspace">
-                <Workspace />
-              </Route>
-              <Route path="/editor">
-                <Editor />
-              </Route>
-            </Switch>
-          </div>
-          <div className="footer">
-            <Footer />
-          </div>
-        </div>
-      </Router>
-    </Suspense>
-  )
+    return (
+        <Suspense fallback={<h1>Loading...</h1>}>
+            <Router>
+                <div className="grid-container">
+                    <div className="nav">
+                        <Nav />
+                    </div>
+                    <div className="sidebar">
+                        <Sidebar />
+                    </div>
+                    <div className="stage">
+                        <Switch>
+                            <Route exact path="/">
+                                <Dashboard />
+                            </Route>
+                            <Route path="/workspace">
+                                <Workspace />
+                            </Route>
+                            <Route path="/editor">
+                                <Editor />
+                            </Route>
+                        </Switch>
+                    </div>
+                    <div className="footer">
+                        <Footer />
+                    </div>
+                </div>
+            </Router>
+        </Suspense>
+    )
 }
